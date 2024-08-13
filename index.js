54c192673ef999fce877e2c5a8dab79e7a533b3e
piano <syedmutahir908@gmail.com>
2024-08-13 06:33:13 -0700

    done

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const port = 5000
 app.get('/', (req, res) => {
     res.send('Hello, World!');
   });
+app.get('/home', (req, res) => {
+    res.send('home page');
+  });
 app.listen(port,()=>{
     console.log(`Server is running on port : ${port}`);
     
