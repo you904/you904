c5e51e9ed0cda706ccfaa8fc7794ccc6738fa7ab
piano <syedmutahir908@gmail.com>
2024-08-13 07:10:25 -0700

    done

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from "express"
-
+import cors from "cors"
 const app = express()
 
 const port = 5000
@@ -8,6 +8,17 @@ app.get('/', (req, res) => {
   });
 app.get('/home', (req, res) => {
     res.send('home page');
+  });
+
+  const corsOptions = {
+    origin: 'https://frontend-lemon-seven.vercel.app/',
+  };
+  
+  app.use(cors(corsOptions));
+  
+  // Your existing routes and middleware
+  app.get('/api/endpoint', (req, res) => {
+    res.json({ message: 'Hello from the backend!' });
   });
 app.listen(port,()=>{
     console.log(`Server is running on port : ${port}`);
